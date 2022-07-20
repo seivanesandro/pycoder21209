bb301b400ba3be0f1e4128c7456e2f8053190688
sandro <seivanesandro@gmail.com>
2022-07-20 18:14:38 +0100

    add docopt to pycoder and call the methods in order for app to work

--- src/pycoder.py
+++ src/pycoder.py
@@ -1,22 +1,55 @@
 """
-PyCoder is an RLE compression/decompression tool.
+Usage:
+    pycoder.py (-c [-t TYPE] | -d) [-p PASSWD] FILE
+    
+Arguments:
+    FILE                            File to be transformed   
 
-This module launches both the PyCoder GUI and the command line
-interfaces. Unlike, gzip/gunzip, pycoder.py acts both as compressor and
-decompressor.
-
-It reads command line arguments using the docopt library and using
-the following grammar:
-
-    $ python3 pycoder.py (-c [-t TYPE] | -d) [-p PASSWD] FILE
-
-This module is also a script and GUI application. Please see function 
-'_main' for instructions on how to use 'pycoder.py' as script or GUI 
-app.
-
-(c) João Galamba, 2022
-$$LICENSE(GPL)
+Options:
+    -c --encode                     Encode FILE optionally with PASSWD
+    -t TYPE --type=TYPE             RLE method [default: 2]
+    -d --decode                     Decode FILE optionally with PASSWD
+    -p PASSWD --password=PASSWD     Encode/Decode using password PASSWD
+    
+    -h --help                       Show this screen
 """
 
+
+from docopt import docopt
+from pathlib import Path
 import rle
+import encrypt
+
+def encodeRLE (type, file):
+    p = Path(file);
+    rle.encode_rle(type, file, p.with_suffix('.rle'))
+    
+def decodeRLE(file):
+    p = Path(file)
+    rle.decode_rle(file, p.with_suffix(''))
+    
+def encodePWD(file, pwd):
+    encrypt.encrypt_file(1, file, pwd)
+    
+def decodePWD(file, pwd):
+    encrypt.decrypt(1,file, file, pwd)
+
+def main():
+    args = docopt(__doc__)
+    
+    if (args['--encode']):
+        if (args['--password']):
+            encodePWD(args['FILE'], args['--password'])
+        else:
+            encodeRLE(args['--type'], args['FILE'])
+    elif (args['--decode']):
+        if (args['--password']):
+            decodePWD(args['FILE'], args['--password'])
+        else:
+            decodeRLE(args['FILE'])
+        
+
+if __name__=='__main__':
+   main()
+    
 
